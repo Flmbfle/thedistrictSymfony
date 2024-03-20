60e62a3a0ded589b6ece98b382eda787838bd9b3
Flmbfle <erwantotet@gmail.com>
2024-03-20 15:48:47 +0100

    Plats Ok

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -7,6 +7,7 @@ use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CatalogueController extends AbstractController
 {
@@ -43,30 +44,18 @@ class CatalogueController extends AbstractController
     public function ViewPlats(): Response
     {
         $categories=$this->categorieRepository->findAll();
-        $plats=$this->platRepository->findAll();
+        $plat=$this->platRepository->findAll();
 
-        return $this->render('plats/index.html.twig', 
+        return $this->render('catalogue/plats.html.twig', 
         
             [
                 'controller_name' => 'PlatsController',
 
                 'categorie'=> $categories,
 
-                'plat'=> $plats
+                'plat'=> $plat
             ]
         );
-    }
-
-    #[Route('/plats/{categorie_id}', name: 'app_platscat')]
-    public function viewPlatCat($categorie_id): Response
-    {
-        // Ici, vous pouvez ajouter votre logique pour récupérer les détails de la catégorie avec l'ID $categorie_id
-
-        // Par exemple, vous pouvez utiliser $categorie_id pour rechercher les plats de cette catégorie dans la base de données
-        
-        return $this->render('catalogue/platscat/' . $categorie_id . '.html.twig', [
-            'categorie_id' => $categorie_id,
-        ]);
     }
 
     #[Route('/categories', name: 'app_categories')]
@@ -84,6 +73,22 @@ class CatalogueController extends AbstractController
                 'categorie'=> $categorie,
             ]
         );
+    }
+
+    #[Route('/plats/{categorie_id}', name: 'app_platscat')]
+    public function viewPlatCat($categorie_id): Response
+    {
+        // Récupérer les détails de la catégorie avec l'ID $categorie_id
+        $categorie = $this->categorieRepository->find($categorie_id);
+        
+        // Récupérer les plats de cette catégorie
+        $plats = $categorie->getPlats();
+    
+        // Passer les plats au template pour les afficher
+        return $this->render('catalogue/platscat/platscat.html.twig', [
+            'categorie' => $categorie,
+            'plats' => $plats,
+        ]);
     }
 
 }
